f039edd3f54455821d6e95b15f50f500623be07e
Grigory Yolkin <gyolkin380@yandex.ru>
2023-10-09 23:18:21 +0300

    first commit

---
+++ frontend/src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './base.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <div>Hello world!</div>
+  </React.StrictMode>,
+)
